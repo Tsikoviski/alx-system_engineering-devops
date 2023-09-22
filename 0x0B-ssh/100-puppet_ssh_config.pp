3d2bf33f6e1ceb9ee5d9ac9eeab705c4413ffa7f
tsikoviski <tsikoviski1@gmail.com>
2023-09-22 12:21:42 -0700

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# modify ssh config file
+exec {'/etc/ssh/ssh_config':
+path     => '/bin',
+command  => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+provider => 'shell',
+}
